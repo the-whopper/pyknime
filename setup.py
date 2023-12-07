0d88e0915d80b6ac2eb719beb35d0064bc163462
the-whopper <79564693+the-whopper@users.noreply.github.com>
2023-12-07 20:35:41 +1100

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='pyknime',
-    version='0.0.0.1',
+    version='0.0.0.2',
     url='https://github.com/the-whopper/pyknime',
     author='Andrew',
     license="MIT",
